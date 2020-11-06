2648a3806a0986d7c69c2665b64e65ea2035c11b
Jamie Kerr <jamiestephenkerr@gmail.com>
2020-11-06 12:41:46 +0000

    Added helper methods for pgp and zip

---
+++ Blaise.Case.Data.Delivery.Tests.Behaviour/Helpers/CompressionHelper.cs
@@ -0,0 +1,15 @@
+﻿using System.IO;
+using Ionic.Zip;
+
+namespace Blaise.Case.Data.Delivery.Tests.Behaviour.Helpers
+{
+    public class CompressionHelper
+    {
+        public void ExtractFileToDirectory(string zipFileName, string outputDirectory)
+        {
+            var zip = ZipFile.Read(zipFileName);
+            Directory.CreateDirectory(outputDirectory);
+            zip.ExtractAll(outputDirectory, ExtractExistingFileAction.OverwriteSilently);
+        }
+    }
+}
