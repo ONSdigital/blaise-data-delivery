a33fb26730d1d3effecce53b4592d7482bfb5ceb
Jamie Kerr <jamiestephenkerr@gmail.com>
2020-11-09 17:55:10 +0000

    Added projects

---
+++ Blaise.Case.Data.Delivery.Data/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Blaise.Case.Data.Delivery.Data
+{
+    public class Class1
+    {
+    }
+}
