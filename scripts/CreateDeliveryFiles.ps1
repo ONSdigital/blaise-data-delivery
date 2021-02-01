37c11803cb1e740195d4ca35d4c444ff1bbb8739
Jamie Kerr <jamiestephenkerr@gmail.com>
2021-02-01 11:58:20 +0000

    Updated env vars

--- scripts/CreateDeliveryFiles.ps1
+++ scripts/CreateDeliveryFiles.ps1
@@ -3,8 +3,8 @@
 ###############################
 
 # If a serverpark is specified then limit the call to that server park
-$catiInstrumentsUri = if([string]::IsNullOrEmpty($env:ENV_SERVER_PARK)) {"$env:ENV_RESTAPI_URL/cati/instruments"} 
-                      else {"$env:ENV_RESTAPI_URL/cati/serverparks/$($env:ENV_SERVER_PARK)/instruments"}
+$catiInstrumentsUri = if([string]::IsNullOrEmpty($env:ServerParkName)) {"$env:ENV_RESTAPI_URL/cati/instruments"} 
+                      else {"$env:ENV_RESTAPI_URL/cati/serverparks/$($env:ServerParkName)/instruments"}
 
 # Retrieve a list of active instruments in CATI for a particular survey type I.E OPN
 $instruments = Invoke-RestMethod -Method Get -Uri $catiInstrumentsUri | where { $_.active -eq $false -and $_.name.StartsWith($env:SURVEY_TYPE) }
@@ -21,13 +21,13 @@ foreach ($instrument in $instruments)
     $InstrumentDataUri = "$($env:ENV_RESTAPI_URL)/serverparks/$($instrument.serverParkName)/instruments/$($instrument.name)/data"
     
     # Build data delivery filename for the instrument
-    $fileName = "dd_$($instrument.name)_$((Get-Date).ToString("ddMMyyyy"))_$((Get-Date).ToString("HHmmss")).$env:PACKAGE_EXTENSION)";
+    $fileName = "dd_$($instrument.name)_$((Get-Date).ToString("ddMMyyyy"))_$((Get-Date).ToString("HHmmss")).$env:PackageExtension)";
 
     # Download instrument package
     wget $InstrumentDataUri -outfile $fileName 
     
     # Upload instrument package to NIFI
-    gsutil cp $fileName gs://$env:BLAISE_NIFI_BUCKET
+    gsutil cp $fileName gs://$env:ENV_BLAISE_NIFI_BUCKET
     
     # remove local instrument package
     Remove-Item $fileName
