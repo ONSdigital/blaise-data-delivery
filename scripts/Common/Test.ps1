766c22361a195f223878eb4235b7055b21cd0371
Jamie Kerr <jamiestephenkerr@gmail.com>
2021-03-25 17:07:47 +0000

    tidied up repo

--- scripts/Common/Test.ps1
+++
@@ -1,5 +0,0 @@
-. "$PSScriptRoot\functions\FileFunctions.ps1"
-
-
-$result = GenerateUniqueFilename -prefix "dd" -instrumentName "OPN2101A" -fileExt "zip"
-Write-Host $result
--- scripts/Opn/DeliverOpnInstruments.ps1
+++ scripts/Opn/DeliverOpnInstruments.ps1
@@ -3,50 +3,37 @@
 ###############################
 
 try {
-    . "$PSScriptRoot\Common\Logging.ps1"
-
-    # If a serverpark is specified then limit the call to that server park
-    $catiInstrumentsUri = if([string]::IsNullOrEmpty($env:ServerParkName)) {"$env:ENV_RESTAPI_URL/api/v1/cati/instruments"} 
-                        else {"$env:ENV_RESTAPI_URL/api/v1/cati/serverparks/$($env:ServerParkName)/instruments"}
+    . "$PSScriptRoot\functions\LoggingFunctions.ps1"
+    . "$PSScriptRoot\functions\FileFunctions.ps1"
+    . "$PSScriptRoot\functions\RestApiFunctions.ps1"
+    . "$PSScriptRoot\functions\SpssFunctions.ps1"
 
     # Retrieve a list of active instruments in CATI for a particular survey type I.E OPN
-    $instruments = Invoke-RestMethod -Method Get -Uri $catiInstrumentsUri | Where-Object { $_.DeliverData -eq $true -and $_.name.StartsWith($env:SurveyType) }
+    $instruments = GetListOfActiveInstruments
 
     # No active instruments found in CATI
     If ($instruments.Count -eq 0) {
         LogWarning("No active instruments found for delivery")
-    }
+    }    
 
     # Deliver the instrument package with data for each active instrument
     foreach ($instrument in $instruments)
     {
-        try {
-            # Build uri to retrive instrument package file with data
-            $InstrumentDataUri = "$($env:ENV_RESTAPI_URL)/api/v1/serverparks/$($instrument.serverParkName)/instruments/$($instrument.name)/data"
-            
-            # Build data delivery filename for the instrument
-            $fileName = "$($instrument.name).$env:PackageExtension"
+        try {           
+            # Generate unique data delivery filename for the instrument
+            $fileName = GenerateUniqueFilename -prefix "dd" -instrumentName $instrument.name
 
-            # Download instrument packagegit pu
-            Invoke-WebRequest $InstrumentDataUri -outfile $fileName 
-            LogInfo("Downloaded instrument '$fileName'")
+            # Download instrument package
+            DownloadInstrumentPackage -serverParkName $instrument.serverParkName -instrumentName $instrument.name -fileName $fileName
 
             # Generate and add SPSS files
-            & .\Common\AddSpssFilesToInstrument.ps1 "$($instrument.name)" | Out-Null
-            LogInfo("Added SPSS files to instrument")
-
-            # Generate DD filename
-            $currentDateTime = (Get-Date)
-            $dataDeliveryFileName = "dd_$($instrument.name)_$($currentDateTime.ToString("ddMMyyyy"))_$($currentDateTime.ToString("HHmmss")).$env:PackageExtension";
-            Rename-Item -Path $fileName -NewName $dataDeliveryFileName
-            LogInfo("Renamed instrument to '$dataDeliveryFileName'")
-         
+            AddSpssFilesToInstrumentPackage -instrumentPackage $fileName -instrumentName $instrument.name 
+        
             # Upload instrument package to NIFI
-            gsutil cp $dataDeliveryFileName gs://$env:ENV_BLAISE_NIFI_BUCKET
-            LogInfo("Pushed instrument '$dataDeliveryFileName' to the NIFI bucket '$($env:ENV_BLAISE_NIFI_BUCKET)'")
+            UploadFileToBucket -fileName $fileName -bucketName $env:ENV_BLAISE_NIFI_BUCKET
 
             # remove local instrument package
-            Remove-Item $dataDeliveryFileName
+            Remove-Item $fileName
         }
         catch {
             LogError($_.ScriptStackTrace)
