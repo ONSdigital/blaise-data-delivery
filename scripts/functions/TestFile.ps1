a6c904c164337df44cab5f115f2581aacc7448f7
jamie kerr <jamiestephenkerr@gmail.com>
2021-03-26 11:24:30 +0000

    updated test file for extract

--- scripts/functions/TestFile.ps1
+++ scripts/functions/TestFile.ps1
@@ -1,6 +1,7 @@
 
 . "$PSScriptRoot\FileFunctions.ps1"
 
-$tempPath = "D:\Opn\Temp"
 $instrumentPackage = "D:\Opn\Temp\OPN2102R.zip"
-AddFilesToZip -files "$tempPath\*.sps","$tempPath\*.asc","$tempPath\*.fps" -zipFilePath $instrumentPackage
+$destinationPath = "D:\Opn\Temp"
+
+ExtractZipFile -zipFilePath instrumentPackage -destinationPath destinationPath
