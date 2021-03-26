d8f96ecfeffe11b2ba17d7a4438cb90ce7b7de46
jamie kerr <jamiestephenkerr@gmail.com>
2021-03-26 11:50:41 +0000

    remove test files

--- scripts/functions/TestFile.ps1
+++
@@ -1,7 +0,0 @@
-
-. "$PSScriptRoot\FileFunctions.ps1"
-
-$instrumentPackage = "D:\Opn\Temp\OPN2102R.zip"
-$destinationPath = "D:\Opn\Temp"
-
-ExtractZipFile -zipFilePath instrumentPackage -destinationPath destinationPath
--- scripts/functions/TestRestApi.ps1
+++
@@ -1,6 +0,0 @@
-
-. "$PSScriptRoot\RestApiFunctions.ps1"
-$instruments = GetListOfActiveInstruments -restApiBaseUrl "http://localhost:90" -surveyType "OPN"
-Write-Host "$($instruments.Count)"
-
-
