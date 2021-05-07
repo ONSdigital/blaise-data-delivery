c4613100299a76ac4bb44b1e23d92a809d15ff0d
Sam Bryant <sam.bryant@armakuni.com>
2021-05-07 14:20:23 +0100

    Break bucket

--- scripts/functions/CloudFunctions.ps1
+++ scripts/functions/CloudFunctions.ps1
@@ -18,7 +18,7 @@ function UploadFileToBucket {
         throw "No delivery zip has been provided" }
 
     LogInfo("Copying '$filePath' to '$bucketName'")
-    $output = & cmd /c "gsutil 2>&1" cp $filePath gs://$bucketName/$deliveryFileName
+    $output = & cmd /c "gsutil 2>&1" cp $filePath gs://foo-$bucketName/$deliveryFileName
     # $output = gsutil cp $filePath gs://$bucketName/$deliveryFileName 2>&1 | %{ "$_" }
     if ($output -Like "*exception*") {
         throw "Failed to upload '$filePath' to '$bucketName': '$output'"
