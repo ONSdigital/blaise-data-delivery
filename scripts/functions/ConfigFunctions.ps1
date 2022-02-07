26a929db37e216fd8eb73ec1bdd05e5d67d864a8
jamie kerr <jamiestephenkerr@gmail.com>
2022-02-07 10:57:39 +0000

    refactor config function

--- scripts/functions/ConfigFunctions.ps1
+++ scripts/functions/ConfigFunctions.ps1
@@ -6,8 +6,16 @@ function GetConfigFromFile {
     $configFile = "$PSScriptRoot\..\configuration\$surveyType.json"
 
     If (Test-Path $configFile) {
-        return Get-Content -Path $configFile | ConvertFrom-Json
+        return ConvertJsonFileToObject($configFile)
     }
 
-    return Get-Content -Path "$PSScriptRoot\..\configuration\default.json" | ConvertFrom-Json
+    return ConvertJsonFileToObject("$PSScriptRoot\..\configuration\default.json")
+}
+
+function ConvertJsonFileToObject {
+    param (
+        [string] $configFile
+    )  
+
+    return Get-Content -Path $configFile | ConvertFrom-Json
 }
