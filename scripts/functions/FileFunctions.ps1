e84dd224c980902596b192e4adfdb291a9b668c0
jamie kerr <jamiestephenkerr@gmail.com>
2024-10-02 12:51:54 +0100

    fix script

--- scripts/functions/FileFunctions.ps1
+++ scripts/functions/FileFunctions.ps1
@@ -144,13 +144,13 @@ function CreateUneditedQuestionnaireFiles {
     }
 
     try {
-        ExtractZipFile -pathTo7zip $pathTo7zip -zipFilePath $deliveryZip -destinationPath $tempPath
+        ExtractZipFile -pathTo7zip $tempPath -zipFilePath $deliveryZip -destinationPath $tempPath
         LogInfo("Extracted the delivery zip")
 
         Rename-Item -Path "$processingFolder\$questionnaireName.bmix" -NewName "$($tempPath)\$($questionnaireName)_UNEDITED.bmix"
         LogInfo("Renamed bmix file")
 
-        AddFilesToZip -pathTo7zip $pathTo7zip -files "$($tempPath)\$($questionnaireName)_UNEDITED.bmix" -zipFilePath $deliveryZip
+        AddFilesToZip -pathTo7zip $tempPath -files "$($tempPath)\$($questionnaireName)_UNEDITED.bmix" -zipFilePath $deliveryZip
         LogInfo("Added bmix file to the delivery zip")
 
     }
