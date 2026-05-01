f84c5d9c9863a1546a2e971f68d7ea1d27834aa7
lambeb <141648830+lambeb@users.noreply.github.com>
2026-05-01 16:51:12 +0100

    Using environmental variable to get project number

--- scripts/download_files_from_shared_bucket.ps1
+++ scripts/download_files_from_shared_bucket.ps1
@@ -57,8 +57,10 @@ try {
     # Write Azure token to disk
     Set-Content -Path $tokenFile -Value $oidcToken
 
+    LogInfo("BENTODO: ENV_BLAISE_SHARED_PROJECT_NUMBER is $env:ENV_BLAISE_SHARED_PROJECT_NUMBER")
+
     # Build WIF Config JSON
-    $audience = "//iam.googleapis.com/projects/894295524217/locations/global/workloadIdentityPools/azdo-identity-pool/providers/azdo-identity-provider"
+    $audience = "//iam.googleapis.com/projects/$env:ENV_BLAISE_SHARED_PROJECT_NUMBER/locations/global/workloadIdentityPools/azdo-identity-pool/providers/azdo-identity-provider"
     $impersonationUrl = "https://iamcredentials.googleapis.com/v1/projects/-/serviceAccounts/${SharedServiceAccount}:generateAccessToken"
 
     $wifConfig = @{
